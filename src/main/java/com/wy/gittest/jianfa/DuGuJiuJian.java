6e3296f4db27b1a7ba386c7ef6fc840a80a977ed
wangyaov587 <wangyaov5@126.com>
2021-06-07 19:03:22 +0800

    update by wangyaov587

---
+++ src/main/java/com/wy/gittest/jianfa/DuGuJiuJian.java
@@ -0,0 +1,12 @@
+package com.wy.gittest.jianfa;
+
+/*
+ * Author :  W.Yao
+ * CRDate :  2021-06-07
+ * Version:  1.0
+ */
+public class DuGuJiuJian {
+    public static void main(String[] args) {
+        System.out.println("独孤九剑，天下无敌~");
+    }
+}
