54b00e407b756d3f01160ab59753292795df117c
wangyaov587 <wangyaov5@126.com>
2021-06-07 21:43:02 +0800

    update by wangyaov587

--- src/main/java/com/wy/gittest/jianfa/DuGuJiuJian.java
+++ src/main/java/com/wy/gittest/jianfa/DuGuJiuJian.java
@@ -8,5 +8,8 @@ package com.wy.gittest.jianfa;
 public class DuGuJiuJian {
     public static void main(String[] args) {
         System.out.println("独孤九剑，天下无敌~");
+
+        // 推送github测试
+        System.out.println("令狐公子，笑傲江湖~");
     }
 }
