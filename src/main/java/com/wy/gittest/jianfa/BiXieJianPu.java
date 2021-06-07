a5404cc81ac4cc2d1174e4adbb30dbfa0b7191fb
wangyaov587 <wangyaov5@126.com>
2021-06-07 21:59:17 +0800

    update by wangyaov587

---
+++ src/main/java/com/wy/gittest/jianfa/BiXieJianPu.java
@@ -0,0 +1,14 @@
+package com.wy.gittest.jianfa;
+
+/*
+ * Author :  W.Yao
+ * CRDate :  2021-06-07
+ * Version:  1.0
+ */
+public class BiXieJianPu {
+    public static void main(String[] args) {
+        System.out.println("辟邪一出，天下混沌~");
+
+        System.out.println("东方教主，一统江湖~");
+    }
+}
